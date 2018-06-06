7ded9d4284b03dee6d747c83f4677abf7bba882f
mokajima <mokajima.85z@gmail.com>
2018-06-06 10:33:11 +0900

    feat: Add an array called allEnemies

--- js/app.js
+++ js/app.js
@@ -44,6 +44,8 @@ Player.prototype.handleInput = function() {
 // Instantiate objects
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+
 var player = new Player();
 
 // This listens for key presses and sends the keys to your
